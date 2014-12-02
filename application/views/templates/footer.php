9e2cb9287dbff5a25fc92b3a10fcf3a30bb4ca5c
Rob Powell <rob_p_powell@yahoo.com>
2014-12-02 18:11:52 -0500

    Added local version of jquery and sticker-nav.css, also got footer to stick

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,13 +1,14 @@
+ </div> <!-- container -->
  <footer class="footer">
       <div class="container">
         <p class="text-muted"> Test Place sticky footer content here.</p>
       </div>
     </footer>
-    </div><!-- /.container -->
     <!-- Bootstrap core JavaScript Test
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+    <script src="<?php echo base_url("assets/js/jquery-1.11.1.min.js");?>"></script>
     <script src="<?php echo base_url("assets/js/bootstrap.min.js");?>"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="<?php echo base_url("assets/js/ie10-viewport-bug-workaround.js");?>"></script>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -15,6 +15,9 @@
 
     <!-- Custom styles for this template -->
     <link href="<?php echo base_url("assets/css/starter-template.css");?>" rel="stylesheet">
+    
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url("assets/css/sticky-footer-navbar.css");?>" rel="stylesheet">    
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="<?php echo base_url("assets/js/ie8-responsive-file-warning.js");?>"></script><![endif]-->
